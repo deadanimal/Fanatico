19b66f947f967474a19009f116812a4cab10f73e
Afeez Aziz <afeezaziz@gmail.com>
2022-11-15 12:05:34 +0800

    123

--- app/Models/GameMatch.php
+++ app/Models/GameMatch.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class GameMatch extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'planned_start_datetime',
+        'planned_end_datetime',
+        'actual_start_datetime',
+        'actual_end_datetime',  
+        'user_id',      
+    ];        
 }
